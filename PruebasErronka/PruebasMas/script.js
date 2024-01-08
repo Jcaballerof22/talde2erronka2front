e5b0d5b6f16d741efb4c8c3ff40b72c7df109627
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2024-01-08 14:16:43 +0100

    aaaaaa

---
+++ PruebasErronka/PruebasMas/script.js
@@ -0,0 +1,24 @@
+function openMenu() {
+    var menuContainer = document.querySelector('.menu-container');
+    var menuTexts = menuContainer.querySelectorAll('.menu-text');
+  
+    menuContainer.style.width = '250px'; // Ancho del menú desplegado
+  
+    // Mostrar todos los textos
+    menuTexts.forEach(function (text) {
+      text.style.display = 'inline';
+    });
+  }
+  
+  function closeMenu() {
+    var menuContainer = document.querySelector('.menu-container');
+    var menuTexts = menuContainer.querySelectorAll('.menu-text');
+  
+    menuContainer.style.width = '100px'; // Ancho inicial del menú
+  
+    // Ocultar todos los textos
+    menuTexts.forEach(function (text) {
+      text.style.display = 'none';
+    });
+  }
+  
