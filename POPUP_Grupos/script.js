8ecde6c0ab6c7e0d12152962a2e910c06dff9e9c
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2024-01-10 13:39:58 +0100

    cambios POPUP-Grupos

--- POPUP_Grupos/script.js
+++ POPUP_Grupos/script.js
@@ -1,7 +1,19 @@
 document.getElementById('mostrarVentana').addEventListener('click', function() {
+    document.getElementById('fondoOscuro').classList.add('mostrar-fondo');
     document.getElementById('ventanaEmergente').style.display = 'block';
 });
 
 document.getElementById('cerrarVentana').addEventListener('click', function() {
-    document.getElementById('ventanaEmergente').style.display = 'none';
+    ocultarVentana();
 });
+
+document.getElementById('fondoOscuro').addEventListener('click', function(event) {
+    if (event.target === this) {
+        ocultarVentana();
+    }
+});
+
+function ocultarVentana() {
+    document.getElementById('fondoOscuro').classList.remove('mostrar-fondo');
+    document.getElementById('ventanaEmergente').style.display = 'none';
+}
