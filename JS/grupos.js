67a1ba265fb84dfcf5c2e2cd0e6552c71599b808
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2024-01-10 10:00:22 +0100

    enseña grupos

--- JS/grupos.js
+++ JS/grupos.js
@@ -1,12 +1,22 @@
 async function a() {
     fetch('http://localhost/Cosos/Erronka/talde2erronka2back/Erronka2/public/api/grupos', { method: 'GET'})
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
             console.log(data); 
-            if (data.match("Fatal error")) {
-                alert("Erregistro hau beste taula batean erabiltzen ari da, beraz, ezin da ezabatu");
-            }else{
-                
+            for (let i = 0; i < data.length; i++) {
+                 // Obtén la referencia de la tabla por su id
+                 var tabla = document.getElementById("tabla");
+
+                 // Agrega una fila a la tabla
+                 var fila = tabla.insertRow();
+ 
+                 // Agrega celdas a la fila
+                 var celdaNombre = fila.insertCell(0);
+                 var celdaNumeroAlumnos = fila.insertCell(1);
+ 
+                 // Agrega contenido a las celdas
+                 celdaNombre.innerHTML = data[i].izena;
+                 celdaNumeroAlumnos.innerHTML = data[i].langileak;
             }
         });
 }
