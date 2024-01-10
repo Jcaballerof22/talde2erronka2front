fb363cd38d7b6295fd801452e684b0e31c93263a
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2024-01-10 13:21:13 +0100

    POPUP-Grupos

---
+++ POPUP_Grupos/script.js
@@ -0,0 +1,7 @@
+document.getElementById('mostrarVentana').addEventListener('click', function() {
+    document.getElementById('ventanaEmergente').style.display = 'block';
+});
+
+document.getElementById('cerrarVentana').addEventListener('click', function() {
+    document.getElementById('ventanaEmergente').style.display = 'none';
+});
