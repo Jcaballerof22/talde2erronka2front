79b691b3fd21ddbef56a74f66a8c4ac87cdc9510
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2024-01-11 08:24:40 +0100

    lomimo

--- JS/grupos.js
+++ JS/grupos.js
@@ -4,19 +4,19 @@ async function a() {
         .then(data => {
             console.log(data); 
             for (let i = 0; i < data.length; i++) {
-                 // Obtén la referencia de la tabla por su id
-                 var tabla = document.getElementById("tabla");
+                // Obtén la referencia de la tabla por su id
+                var tabla = document.getElementById("tabla");
 
-                 // Agrega una fila a la tabla
-                 var fila = tabla.insertRow();
- 
-                 // Agrega celdas a la fila
-                 var celdaNombre = fila.insertCell(0);
-                 var celdaNumeroAlumnos = fila.insertCell(1);
- 
-                 // Agrega contenido a las celdas
-                 celdaNombre.innerHTML = data[i].izena;
-                 celdaNumeroAlumnos.innerHTML = data[i].langileak;
+                // Agrega una fila a la tabla
+                var fila = tabla.insertRow();
+    
+                // Agrega celdas a la fila
+                var celdaNombre = fila.insertCell(0);
+                var celdaNumeroAlumnos = fila.insertCell(1);
+    
+                // Agrega contenido a las celdas
+                celdaNombre.innerHTML = data[i].izena;
+                celdaNumeroAlumnos.innerHTML = data[i].langileak;
             }
         });
 }
