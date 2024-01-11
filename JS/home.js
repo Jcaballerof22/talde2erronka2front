9cc793c18497d1ee73e574a9b59478d5854aa15d
Jcaballerof22 <jcaballerof22@fpsanjorge.com>
2024-01-11 09:51:10 +0100

    Editar horarios home.html

--- JS/home.js
+++ JS/home.js
@@ -3,3 +3,19 @@ document.getElementById('editarHorarios').addEventListener('click', function() {
     document.getElementById('fondoOscuro').classList.add('mostrar-fondo');
     // window.alert('aaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAAA');
 })
+
+document.getElementById('cerrarHorarios').addEventListener('click', function() {
+    document.getElementById('fondoOscuro').classList.remove('mostrar-fondo');
+    document.getElementById('tablaHorarios').style.display = 'none';
+});
+
+document.getElementById('fondoOscuro').addEventListener('click', function(event) {
+    if (event.target === this) {
+        ocultarVentana();
+    }
+});
+
+function ocultarVentana() {
+    document.getElementById('fondoOscuro').classList.remove('mostrar-fondo');
+    document.getElementById('tablaHorarios').style.display = 'none';
+}
