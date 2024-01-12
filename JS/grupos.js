2d698767238d11dab2937a4afc7979ed33d57ea4
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2024-01-12 10:03:23 +0100

    grupos pasado a vue

--- JS/grupos.js
+++ JS/grupos.js
@@ -1,28 +1,51 @@
-async function a() {
-    fetch('../../talde2erronka2back/Erronka2/public/api/grupos', { method: 'GET'})
+var app = new Vue({
+    el: '#app',
+    data: {
+        izena: "",
+        kodea: "",
+        datos: [],
+    },
+    methods: {
+        addDatos(){
+            //
+                if(this.kodea=="" || this.izena==""){
+                    alert("Datu falta dira")
+                }else{
+                    var js = JSON.stringify({"kodea": this.kodea, "izena": this.izena}); 
+                    console.log("froga: "+js);
+                    fetch('../../talde2erronka2back/Erronka2/public/api/grupos/txertatu', {method: 'POST', body: js})
+                    .then(function (response) {
+                            return response.text();
+                    })
+                    .then(data=>{
+                        console.log(data);
+                        this.datos.push({"izena" : this.izena, "langileKop" : "0"});
+                        
+                    })
+                    .catch(error => {
+                        console.log("Erregistro hau beste taula batean erabiltzen ari da, beraz, ezin da ezabatu" + error);
+                    });
+            //
+                }
+        }
+    },
+    watch:{},
+    mounted: function() {
+        // Código que se ejecuta cuando la instancia Vue se ha montado en el DOM
+        console.log('La instancia Vue se ha montado en el DOM.');
+        // Puedes realizar operaciones adicionales aquí
+        fetch('../../talde2erronka2back/Erronka2/public/api/grupos', { method: 'GET'})
         .then(response => response.json())
         .then(data => {
             console.log(data); 
             for (let i = 0; i < data.length; i++) {
                 // Obtén la referencia de la tabla por su id
-                var tabla = document.getElementById("tabla");
-
-                // Agrega una fila a la tabla
-                var fila = tabla.insertRow();
-    
-                // Agrega celdas a la fila
-                var celdaNombre = fila.insertCell(0);
-                var celdaNumeroAlumnos = fila.insertCell(1);
-    
-                // Agrega contenido a las celdas
-                celdaNombre.innerHTML = data[i].izena;
-                celdaNumeroAlumnos.innerHTML = data[i].langileak;
+                this.datos.push({"izena" : data[i].izena, "langileKop" : data[i].langileak});
             }
         });
-}
+      }
+});
 
-a();
-// 
 
 document.getElementById('mostrarVentanaGrupos').addEventListener('click', function() {
     document.getElementById('fondoOscuroGrupos').classList.add('mostrar-fondo');
@@ -42,36 +65,4 @@ document.getElementById('fondoOscuroGrupos').addEventListener('click', function(
 function ocultarVentana() {
     document.getElementById('fondoOscuroGrupos').classList.remove('mostrar-fondo');
     document.getElementById('ventanaEmergenteGrupos').style.display = 'none';
-}
-
-function insertar(){
-    kodea = document.getElementById('codigoTextoGrupos').value;
-    izena = document.getElementById('nombreTextoGrupos').value;
-    if(kodea=="" || izena==""){
-        alert("Datu falta dira")
-    }else{
-        var datos = {kodea, izena};
-        var js = JSON.stringify(datos); 
-        console.log("froga: "+js);
-
-        fetch('../../talde2erronka2back/Erronka2/public/api/grupos/txertatu', {method: 'POST', body: js})
-        .then(function (response) {
-                return response.text();
-        })
-        .then(data=>{
-            console.log(data);
-            // if(data.match("No hay stock suficiente")){
-            //     if(confirm("No puedes insertar más stock. Deseas añadir?")){
-            //         gehituStock();
-            //     }else{
-
-            //     }
-            //     alert(etiketa + " txertatuta");
-            // }
-            //AQUÍ UN WINDOW PROMPT DEL STOCK DISPONIBLE DE ESE EKIPAMENDU
-        })
-        .catch(error => {
-            console.log("Erregistro hau beste taula batean erabiltzen ari da, beraz, ezin da ezabatu" + error);
-        });
-    }
 }
