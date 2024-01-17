d1b417167106a182d6ddb51a6e57f61355a54b99
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2024-01-17 12:22:34 +0100

    POPUPS

--- CitasPost/script.js
+++ CitasPost/script.js
@@ -17,8 +17,3 @@ function ocultarVentana() {
     document.getElementById('fondoOscuroGrupos').classList.remove('mostrar-fondo');
     document.getElementById('ventanaEmergenteGrupos').style.display = 'none';
 }
-
-function seleccionarOpcion(texto) {
-    var dropdownButton = document.getElementById("miDropdown");
-    dropdownButton.innerHTML = texto + ' <span class="caret"></span>';
-}
---
+++ ReservarMaterial/script.js
@@ -0,0 +1,20 @@
+document.getElementById('mostrarVentanaLangile').addEventListener('click', function() {
+    document.getElementById('fondoOscuroLangile').classList.add('mostrar-fondo');
+    document.getElementById('ventanaEmergenteLangile').style.display = 'block';
+});
+
+document.getElementById('cerrarVentanaLangile').addEventListener('click', function() {
+    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
+    document.getElementById('ventanaEmergenteLangile').style.display = 'none';
+});
+
+document.getElementById('fondoOscuroLangile').addEventListener('click', function(event) {
+    if (event.target === this) {
+        ocultarVentana();
+    }
+});
+
+function ocultarVentana() {
+    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
+    document.getElementById('ventanaEmergenteLangile').style.display = 'none';
+}
