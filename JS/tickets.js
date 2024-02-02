b8644c77a0f4fd22ac74a339d3640cb0a5216584
Jcaballerof22 <jcaballerof22@fpsanjorge.com>
2024-02-02 14:18:16 +0100

    Fichas + tickets

--- JS/tickets.js
+++ JS/tickets.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        datosTickets: []
+        datosTickets: [],
+        taula: []
     },
     methods: {
         async mostrarTickets() {
@@ -20,13 +21,45 @@ var app = new Vue({
                   "data": data[i].data,
                   "tratamendu_izena": data[i].tratamendu_izena,
                   "prezioa": data[i].prezioa,
+                  "id": data[i].id
+                });
+
+                this.taula.push({
+                    "bezero_izena": data[i].bezero_izena,
+                    "data": data[i].data,
+                    "tratamendu_izena": data[i].tratamendu_izena,
+                    "prezioa": data[i].prezioa,
+                    "id": data[i].id
                 });
               }
             } catch (error) {
               console.error('Error al obtener datos del servidor:', error);
             }
         },
-
+        async ezabatu(kodea, index) {
+            const js = JSON.stringify({"id": kodea}); 
+            console.log("froga: " + js);
+  
+            this.taula.splice(index, 1);
+            for (let i = 0; i < this.taula.length; i++) {
+                console.log(this.taula[i]);;    
+            }
+            
+            
+            try {
+                const response = await fetch('../../talde2erronka2back/Erronka2/public/api/tickets/ezabatu', {
+                    method: 'PUT',
+                    body: js
+                });
+  
+                const data = await response.text();
+                console.log(data);
+  
+            } catch (error) {
+                console.error("Error al eliminar el registro:", error);
+                console.log("El registro ya está siendo utilizado en otra tabla, por lo tanto, no se puede eliminar.");
+            }
+        },
         abrirPopup(kodea, izena, abizenak, id){
             //lodelPopup
             this.aldatu = id;
@@ -36,8 +69,12 @@ var app = new Vue({
             this.id = id
              
             document.getElementById('fondoOscuroLangile').classList.add('mostrar-fondo');
-            document.getElementById('ventanaEmergenteLangile').style.display = 'block';
+            document.getElementById('ventanaEmergenteTickets').style.display = 'block';
         },
+        cerrarPopup(){
+            document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
+            document.getElementById('ventanaEmergenteTickets').style.display = 'none';
+          },
         buscar(){
             if (this.bilatu == ''){
                 this.taula = this.datos;
@@ -53,103 +90,34 @@ var app = new Vue({
         
     },
     watch:{
-        bilatu: function(){
-            if (this.bilatu == ''){
-                this.taula = this.datos;
-            }else{
-                this.taula = [];
-                for (let i = 0; i < this.datos.length; i++){
-                    if(this.datos[i].izena.startsWith(this.bilatu)){
-                        this.taula.push({"izena" : this.datos[i].izena, "abizenak" : this.datos[i].abizenak, "kodea" : this.datos[i].kodea, "id" : this.datos[i].id});
-                    }
-                }
-            }
-        },
+        // bilatu: function(){
+        //     if (this.bilatu == ''){
+        //         this.taula = this.datos;
+        //     }else{
+        //         this.taula = [];
+        //         for (let i = 0; i < this.datos.length; i++){
+        //             if(this.datos[i].izena.startsWith(this.bilatu)){
+        //                 this.taula.push({"izena" : this.datos[i].izena, "abizenak" : this.datos[i].abizenak, "kodea" : this.datos[i].kodea, "id" : this.datos[i].id});
+        //             }
+        //         }
+        //     }
+        // },
 
-        sailkatu: function(){
-            if (this.sailkatu == 'all'){
-                this.taula = this.datos;
-            }else{
-                this.taula = [];
-                for (let i = 0; i < this.datos.length; i++){
-                    if(this.datos[i].kodea == this.sailkatu){
-                        this.taula.push({"izena" : this.datos[i].izena, "abizenak" : this.datos[i].abizenak, "kodea" : this.datos[i].kodea, "id" : this.datos[i].id});
-                    }
-                }
-            }
-        }
+        // sailkatu: function(){
+        //     if (this.sailkatu == 'all'){
+        //         this.taula = this.datos;
+        //     }else{
+        //         this.taula = [];
+        //         for (let i = 0; i < this.datos.length; i++){
+        //             if(this.datos[i].kodea == this.sailkatu){
+        //                 this.taula.push({"izena" : this.datos[i].izena, "abizenak" : this.datos[i].abizenak, "kodea" : this.datos[i].kodea, "id" : this.datos[i].id});
+        //             }
+        //         }
+        //     }
+        // }
     },
     mounted: function() {
         this.mostrarTickets();
     }
 });
-
-// Primer POPUP
-
-document.getElementById('ticket').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.add('mostrar-fondo');
-    document.getElementById('ventanaEmergenteTicket').style.display = 'block';
-});
-
-document.getElementById('cerrarVentanaTicket').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteTicket').style.display = 'none';
-});
-
-document.getElementById('fondoOscuroLangile').addEventListener('click', function(event) {
-    if (event.target === this) {
-        ocultarVentana();
-    }
-});
-
-function ocultarVentana() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteTicket').style.display = 'none';
-}
-
-// Segundo POPUP  TODAVIA POR ARREGLAR
-
-document.getElementById('ticketMirar').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.add('mostrar-fondo');
-    document.getElementById('ventanaEmergenteLangile').style.display = 'block';
-});
-
-document.getElementById('cerrarVentanaTicketMirar').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteLangile').style.display = 'none';
-});
-
-document.getElementById('fondoOscuroLangile').addEventListener('click', function(event) {
-    if (event.target === this) {
-        ocultarVentanaMirar();
-    }
-});
-
-function ocultarVentanaMirar() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteLangile').style.display = 'none';
-}
-
-// CitasPost
-
-document.getElementById('citasPost').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.add('mostrar-fondo');
-    document.getElementById('ventanaEmergenteCitasPost').style.display = 'block';
-});
-
-document.getElementById('cerrarVentanaCitasPost').addEventListener('click', function() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteCitasPost').style.display = 'none';
-});
-
-document.getElementById('fondoOscuroLangile').addEventListener('click', function(event) {
-    if (event.target === this) {
-        ocultarVentana();
-    }
-});
-
-function ocultarVentana() {
-    document.getElementById('fondoOscuroLangile').classList.remove('mostrar-fondo');
-    document.getElementById('ventanaEmergenteCitasPost').style.display = 'none';
-}
 
