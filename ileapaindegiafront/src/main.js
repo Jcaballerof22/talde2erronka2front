e38307157f98e5645c405413c6c31461a7919d09
izasfernandez <ifernandezi22@fpsanjorge.com>
2024-02-27 10:19:31 +0100

    bootstrap

--- ileapaindegiafront/src/main.js
+++ ileapaindegiafront/src/main.js
@@ -1,9 +1,10 @@
-import './assets/styles.css';
 import router from './router';
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './assets/styles.css';
 
 
 const app = createApp(App);
