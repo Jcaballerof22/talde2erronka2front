4243d9b4412dc52581f32cf3612fdd8371fcf786
ZureIzena <zure.email@example.com>
2024-05-17 13:57:58 +0200

    .

--- ileapaindegiafront/src/App.vue
+++ ileapaindegiafront/src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
   import {ref} from "vue";
   import Menu from './components/Menu.vue'
-  // window.ruta = 'http://localhost/talde2erronka2back/Erronka2/public/api/';
-  window.ruta = 'http://localhost/talde2erronka2back/Erronka2/public/api/';
+  window.ruta = 'http://localhost/Cosos/KK/Erronka/talde2erronka2back/Erronka2/public/api/';
   window.jsones = 'http://localhost/Cosos/KK/Erronka/talde2erronka2front/ileapaindegiafront/src/assets/json/';
+  // window.ruta = 'http://localhost/talde2erronka2back/Erronka2/public/api/';
   //Funtzioak
   const translate = (hizkuntza) => {
     sessionStorage.setItem("hizkuntza", hizkuntza);
