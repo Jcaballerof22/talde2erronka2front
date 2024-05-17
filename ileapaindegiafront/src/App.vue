11252ab9536ef8a0f05f8b7efbecf9a417425bcd
ZureIzena <zure.email@example.com>
2024-05-17 13:59:09 +0200

    .

--- ileapaindegiafront/src/App.vue
+++ ileapaindegiafront/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import {ref} from "vue";
   import Menu from './components/Menu.vue'
+  // EN VEZ DE CAMBIAR MI RUTA, PLS DESCOMENTA LA DE ABAJO <3
   window.ruta = 'http://localhost/Cosos/KK/Erronka/talde2erronka2back/Erronka2/public/api/';
   window.jsones = 'http://localhost/Cosos/KK/Erronka/talde2erronka2front/ileapaindegiafront/src/assets/json/';
   // window.ruta = 'http://localhost/talde2erronka2back/Erronka2/public/api/';
