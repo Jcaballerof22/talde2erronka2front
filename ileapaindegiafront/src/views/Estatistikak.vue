3ce32edc72fa5fbd54f9744a2a2ed6a3420cc290
JON\jonca <jcaballerof22@fpsanjorge.com>
2024-05-22 19:17:20 +0200

    Estadísticas definitivo

--- ileapaindegiafront/src/views/Estatistikak.vue
+++ ileapaindegiafront/src/views/Estatistikak.vue
@@ -10,6 +10,10 @@
             alumnosNombre: [],
             alumnosL: [],
             alumnosM: [],
+            alumnosFecha: [],
+            nombreAlumnos: [],
+            LAlumnos: [],
+            MAlumnos: [],
             productosNombre: [],
             productosNumero: [],
             productosFecha: [],
@@ -17,6 +21,9 @@
             numeroProductos: [],
             materialNombre: [],
             materialNumero: [],
+            materialFecha: [],
+            nombreMaterial: [],
+            numeroMaterial: [],
             datosTalde: [],
             fechaInicio: "",
             fechaFin: "",
@@ -38,16 +45,57 @@
             }, 500); // Retraso de 1000 milisegundos
         },
         // Rolen grafikoa sortzeko metodoa
-        graficoRoles(){
+        graficoRoles() {
             this.titulo2 = testua[this.hizkuntza]['Roles'];
             this.grafico = "graficoRoles";
 
+            // Limpiar los arrays antes de llenarlos nuevamente
+            this.nombreAlumnos = [];
+            this.LAlumnos = [];
+            this.MAlumnos = [];
+
+            // Usar un objeto para acumular los alumnos y sus roles
+            const alumnosAcumulados = {};
+
+            for (let i = 0; i < this.alumnosFecha.length; i++) {
+                const fechaActual = new Date(this.alumnosFecha[i]);
+                const fechaInicio = this.fechaInicio ? new Date(this.fechaInicio) : null;
+                const fechaFin = this.fechaFin ? new Date(this.fechaFin) : null;
+
+                // Filtrar según las fechas
+                if ((fechaInicio && fechaActual < fechaInicio) || (fechaFin && fechaActual > fechaFin)) {
+                    continue; // Saltar las fechas fuera del rango
+                }
+
+                // Acumular los alumnos por nombre
+                const nombreAlumno = this.alumnosNombre[i];
+                const cantidadL = parseFloat(this.alumnosL[i]);
+                const cantidadM = parseFloat(this.alumnosM[i]);
+
+                if (alumnosAcumulados[nombreAlumno]) {
+                    alumnosAcumulados[nombreAlumno].cantidadL += cantidadL;
+                    alumnosAcumulados[nombreAlumno].cantidadM += cantidadM;
+                } else {
+                    alumnosAcumulados[nombreAlumno] = { cantidadL: cantidadL, cantidadM: cantidadM };
+                }
+            }
+
+            // Convertir el objeto acumulador en arrays para el gráfico
+            for (const nombre in alumnosAcumulados) {
+                if (alumnosAcumulados.hasOwnProperty(nombre)) {
+                    this.nombreAlumnos.push(nombre);
+                    this.LAlumnos.push(alumnosAcumulados[nombre].cantidadL);
+                    this.MAlumnos.push(alumnosAcumulados[nombre].cantidadM);
+                }
+            }
+
+            // Configuración del gráfico
             const data = {
-                labels: this.alumnosNombre,
+                labels: this.nombreAlumnos,
                 datasets: [
                     {
                         label: testua[this.hizkuntza]['Limpieza'],
-                        data: this.alumnosL,
+                        data: this.LAlumnos,
                         borderColor: 'rgb(0, 128, 128)',
                         backgroundColor: 'rgb(26, 183, 188, 0.5)',
                         borderWidth: 2,
@@ -56,7 +104,7 @@
                     },
                     {
                         label: testua[this.hizkuntza]['Recepcion'],
-                        data: this.alumnosM,
+                        data: this.MAlumnos,
                         borderColor: 'green',
                         backgroundColor: 'rgba(205, 223, 160, 0.5)',
                         borderWidth: 2,
@@ -65,7 +113,7 @@
                     }
                 ]
             };
-              
+
             const config = {
                 type: 'bar',
                 data: data,
@@ -78,7 +126,7 @@
                     }
                 },
             };
-              
+
             const ctx = document.getElementById('grafikoa').getContext('2d');
 
             if (this.myChart) {
@@ -88,15 +136,53 @@
             this.myChart = new Chart(ctx, config);
         },
         // Materialen grafikoa sortzeko metodoa
-        graficoMaterial(){
+        graficoMaterial() {
             this.titulo2 = testua[this.hizkuntza]['Material'].toUpperCase();
             this.grafico = "graficoMaterial";
+
+            // Limpiar los arrays antes de llenarlos nuevamente
+            this.nombreMaterial = [];
+            this.numeroMaterial = [];
+
+            // Usar un objeto para acumular los materiales y sus cantidades
+            const materialesAcumulados = {};
+
+            for (let i = 0; i < this.materialFecha.length; i++) {
+                const fechaActual = new Date(this.materialFecha[i]);
+                const fechaInicio = this.fechaInicio ? new Date(this.fechaInicio) : null;
+                const fechaFin = this.fechaFin ? new Date(this.fechaFin) : null;
+
+                // Filtrar según las fechas
+                if ((fechaInicio && fechaActual < fechaInicio) || (fechaFin && fechaActual > fechaFin)) {
+                    continue; // Saltar las fechas fuera del rango
+                }
+
+                // Acumular los materiales por nombre
+                const nombreMaterial = this.materialNombre[i];
+                const numeroMaterial = parseFloat(this.materialNumero[i]);
+
+                if (materialesAcumulados[nombreMaterial]) {
+                    materialesAcumulados[nombreMaterial] += numeroMaterial;
+                } else {
+                    materialesAcumulados[nombreMaterial] = numeroMaterial;
+                }
+            }
+
+            // Convertir el objeto acumulador en arrays para el gráfico
+            for (const nombre in materialesAcumulados) {
+                if (materialesAcumulados.hasOwnProperty(nombre)) {
+                    this.nombreMaterial.push(nombre);
+                    this.numeroMaterial.push(materialesAcumulados[nombre]);
+                }
+            }
+
+            // Configuración del gráfico
             const data = {
-                labels: this.materialNombre,
+                labels: this.nombreMaterial,
                 datasets: [
                     {
                         label: testua[this.hizkuntza]['Cantidad'],
-                        data: this.materialNumero,
+                        data: this.numeroMaterial,
                         borderColor: 'green',
                         backgroundColor: 'rgba(205, 223, 160, 0.5)',
                         borderWidth: 2,
@@ -105,7 +191,7 @@
                     }
                 ]
             };
-              
+
             const config = {
                 type: 'bar',
                 data: data,
@@ -118,14 +204,16 @@
                     }
                 },
             };
-              
+
             const ctx = document.getElementById('grafikoa').getContext('2d');
-              
+
+            // Destruir el gráfico anterior si existe
             if (this.myChart) {
                 this.myChart.destroy();
             }
 
-            this.myChart = new Chart(ctx, config);               
+            // Crear un nuevo gráfico
+            this.myChart = new Chart(ctx, config);            
         },
         // Produktuen grafikoa sortzeko metodoa
         graficoProductos() {
@@ -238,12 +326,18 @@
               }
           
               const data = await response.json();
+
+                this.alumnosNombre=[];
+                this.alumnosL=[];
+                this.alumnosM=[];
+                this.alumnosFecha=[];
           
               if (data && data.length > 0) { // Verificar si hay datos antes de continuar
                 for (let i = 0; i < data.length; i++) {
                     this.alumnosNombre.push(data[i].izena + " " + data[i].abizenak);
                     this.alumnosL.push(data[i].suma_m);
                     this.alumnosM.push(data[i].suma_g);
+                    this.alumnosFecha.push(data[i].sortze_data);
                 }
 
                 // if (this.myChart) {
@@ -296,6 +390,7 @@
               for (let i = 0; i < data.length; i++) {
                 this.materialNombre.push(data[i].izena + " " + data[i].etiketa);
                 this.materialNumero.push(data[i].count_id);
+                this.materialFecha.push(data[i].hasiera_data);
               }
             } catch (error) {
               console.error('Error al obtener datos del servidor:', error);
@@ -321,7 +416,13 @@
         },
         abrirpdf(){
             window.open('http://localhost/talde2erronka2front/ileapaindegiafront/src/IMG/Manual%20de%20Usuario.pdf#page=12', '_blank');
-        }     
+        },
+        prueba(){
+            // this.grupoHoy = 'PEL2';
+            this.sacarAlumnos().then(() => {
+                this.graficoRoles();
+            });
+        }
     },
     mounted: function(){
         this.hizkuntza = this.hizkuntzaLortu()
@@ -336,7 +437,6 @@
     },
     watch:{
         fechaInicio: function(){
-            console.log("Cambia"); 
             this.esperar(this.grafico);
         },
 
@@ -357,6 +457,9 @@
                     <button type="button" class="btn añadir btn-lg" @click="esperar('graficoRoles')">{{testua[hizkuntza]?.['Roles']}}</button>
                     <button type="button" class="btn añadir btn-lg" @click="esperar('graficoProductos')">{{testua[hizkuntza]?.['Productos']}}</button>
                     <button type="button" class="btn añadir btn-lg" @click="esperar('graficoMaterial')">{{testua[hizkuntza]?.['Material']}}</button>
+                    <select class=" combobox" id="selectorLangilea" aria-label="Default select example" v-model="grupoHoy" @change="prueba()">
+                                <option   v-for="(dato,index) in datosTalde" :key="index" :value="dato.izena">{{ dato.izena }}</option>
+                            </select>
                     <!-- <select class="form-select combobox" aria-label="Default select example" v-model="grupoHoy" @change="sacarAlumnos">
                         <option v-for="talde in datosTalde" :value="talde.izena">{{ talde.izena }}</option>
                     </select> -->
@@ -364,7 +467,7 @@
                 <!-- Dataren eta izenaren arabera bilatzeko filtroak -->
                 <div class="col">
                     <div class="input-group">
-                        <div class="me-4">
+                        <div class="row me-4">
                             <h4>
                             <input type="date" id="birthday" name="birthday" v-model="fechaInicio">
                             ⇨
